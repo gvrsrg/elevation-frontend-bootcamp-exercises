2c9f9607a10f2def3ca1b3859e7b01e2b450b855
gvrsrg <gvrsrg@gmail.com>
2023-06-21 18:56:55 +0300

    mathOperations

---
+++ week-04/day03/mathOperations.js
@@ -0,0 +1,33 @@
+const mathOperations = function () {
+    const add = function (x, y) {
+        return x + y;
+    }
+
+    const subtract = function (x, y) {
+        return x - y;
+    }
+
+    const multiply = function (x, y) {
+        return x * y;
+    }
+
+    const divide = function (x, y) {
+        return x / y;
+    }
+
+    return {
+        add: add,
+        sub: subtract,
+        mult: multiply,
+        div: divide
+    }
+}
+
+const m = mathOperations()
+
+console.log(m.add(13, 29))
+
+console.log(m.mult(1.75, 24))
+
+console.log(m.mult(7, m.div(36, 6)))
+
