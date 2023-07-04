b69fd2c22ab8756ad8c43e7e937a1b1bd395db48
gvrsrg <gvrsrg@gmail.com>
2023-07-04 15:23:05 +0300

    DOM Traversal exercise

--- week-06/day01/main.js
+++ week-06/day01/main.js
@@ -10,11 +10,28 @@ const generator = function () {
     console.log(`Computer’s data-id: ${divComputer.attr("data-id")}`)
     console.log(`BUS: ${divBus[0].innerText}`)
 
-
-
-
-
 }
 
+const validator = function () {
+    divComputer = $(this).closest(".computer")
+    divProcessor = divComputer.find(".processor")
+    buttonGenerator = divComputer.find("button.generator")
+    pMB = divComputer.find("p.MB")
+    spanQR = divProcessor.find(".QR")
+    divBus = divComputer.find("p.BUS")
+    let textQR = ""
+    for (let QR of spanQR) {
+        textQR += QR.innerText +", "
+    }
+    textQR = textQR.slice(0, -2);
+    console.log(`The generator’s text: ${buttonGenerator[0].innerText}`)
+    console.log(`MB: ${pMB[0].innerText}`)
+    console.log(`QR: ${textQR}`)
+
+}
+// The generator’s text
+// The MB
+// Both QRs
 
 $(".generator").on("click", generator); 
+$(".validator").on("click", validator); 
