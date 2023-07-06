750581573a9c3761b77c5249eab4f46ba360681e
gvrsrg <gvrsrg@gmail.com>
2023-07-06 19:01:27 +0300

    Handlebars

--- week-06/day02/main.js
+++ week-06/day02/main.js
@@ -45,3 +45,25 @@ const menuData = {
   
   // append our new html to the page
   $('.menu').append(menuNewHTML);
+
+
+const animals = ["Rabbit", "Giraffe", "Kangaroo", "Whale", "Seagull", "Caterpie"]
+
+const languages = ["French", "Spanish", "Togolese", "Javascript", "Uruk"]
+
+const renderAnimals = function(){
+    const source = $('#animals-template').html();
+    const template = Handlebars.compile(source);
+    let newHTML = template(animals);
+    $('#animals').append(newHTML);  
+  }
+  
+  const renderLanguages = function(){
+    const source = $('#languages-template').html();
+    const template = Handlebars.compile(source);
+    let newHTML = template(languages);
+    $('#languages').append(newHTML);  
+  }
+
+  renderAnimals()
+  renderLanguages()
