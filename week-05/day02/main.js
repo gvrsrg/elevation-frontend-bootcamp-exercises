6d7635bcd46d52fb65999ce0341b9d473981f42c
gvrsrg <gvrsrg@gmail.com>
2023-07-18 19:41:43 +0300

    1

--- week-05/day02/main.js
+++ week-05/day02/main.js
@@ -1,2 +1,5 @@
-const header = $("h1")
-console.log(header)
+$("h1").css("background-color", "blue")
+$(".red-div").css("background-color", "red")
+$("li:first-child").css("background-color", "green")
+$("li:last-child").css("background-color", "pink")
+$("#brown-div").css("background-color", "brown")
