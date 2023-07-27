30b3d60569f501a0b4ec9baac017175ec98c7441
gvrsrg <gvrsrg@gmail.com>
2023-07-27 15:37:40 +0300

    exercise 2 React Intro

--- week-09/react/lesson/src/App.js
+++ week-09/react/lesson/src/App.js
@@ -3,18 +3,31 @@ import './App.css';
 
 function App() {
 
-
-  let companies = [
-    { name: "Tesla", revenue: 140 },
-    { name: "Microsoft", revenue: 300 },
-    { name: "Google", revenue: 600 }
-  ]
+//exercise 1 React Intro
+  // let companies = [ 
+  //   { name: "Tesla", revenue: 140 },
+  //   { name: "Microsoft", revenue: 300 },
+  //   { name: "Google", revenue: 600 }
+  // ]
   
-  const showCompany = (name, revenue) => <div id={name}>{name} makes {revenue} every year</div>
+  // const showCompany = (name, revenue) => <div id={name}>{name} makes {revenue} every year</div>
 
-  return (
-    companies.map((c) => showCompany(c.name, c.revenue))
-  )
+  // return (
+  //   companies.map((c) => showCompany(c.name, c.revenue))
+  // )
+//exercise 2 React Intro 
+  const getClassName = (temperature) => {
+    if (temperature < 15) return "freezing";
+    if (temperature > 30) return "hell-scape";
+    return "fair";
+  }
+
+  const temperature = Math.floor(Math.random()*100);
+
+ return (
+       <div id="weatherBox" className={getClassName(temperature)}>{temperature} deg</div>
+ )
+
 }
 
 export default App;
