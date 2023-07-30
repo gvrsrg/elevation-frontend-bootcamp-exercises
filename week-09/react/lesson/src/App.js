7cc1ee3a5f0139084e90db45dd00b5f903c952b9
gvrsrg <gvrsrg@gmail.com>
2023-07-30 23:14:42 +0300

    react lesson

--- week-09/react/lesson/src/App.js
+++ week-09/react/lesson/src/App.js
@@ -1,8 +1,65 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function Nav() {
+  return (
+      <div id="nav"><span>Home</span><span>About</span></div>
+  )
+}
 
+function AboutUs() {
+  return <p>This is an example of an about us page~</p>
+}
+
+export function Sum() {
+  const num1 = 9
+  const num2 = 3
+  return (
+      <div>
+          The sum is {num1 + num2}
+      </div>
+  )
+}
+
+function LandingPage() {
+  return (
+    <div>Welcome!</div>
+  )
+}
+
+function About () {
+  return (<div><p>We are great!</p>
+    <SignUp />
+    <Blurb />
+    </div>
+
+  )
+}
+
+
+function SignUp (){
+  return <p>Sign up here...</p>
+}
+
+function Blurb (){
+  return <p>Blurb...</p>
+}
+
+function App() {
+  
+  let isUserLoggedIn = localStorage.getItem('loggedIn')
+  let componentToDisplay = isUserLoggedIn ?
+      <LandingPage /> :
+      <AboutUs />
+
+  return (
+      <div className="app">
+          <Nav />
+          <About />
+          <LandingPage />
+      </div>
+  )
+  
 //exercise 1 React Intro
   // let companies = [ 
   //   { name: "Tesla", revenue: 140 },
@@ -16,18 +73,20 @@ function App() {
   //   companies.map((c) => showCompany(c.name, c.revenue))
   // )
 //exercise 2 React Intro 
-  const getClassName = (temperature) => {
-    if (temperature < 15) return "freezing";
-    if (temperature > 30) return "hell-scape";
-    return "fair";
-  }
+//   const getClassName = (temperature) => {
+//     if (temperature < 15) return "freezing";
+//     if (temperature > 30) return "hell-scape";
+//     return "fair";
+//   }
 
-  const temperature = Math.floor(Math.random()*100);
+//   const temperature = Math.floor(Math.random()*100);
 
- return (
-       <div id="weatherBox" className={getClassName(temperature)}>{temperature} deg</div>
- )
-
+//  return (
+//        <div id="weatherBox" className={getClassName(temperature)}>{temperature} deg</div>
+//  )
 }
+
+
+
 
 export default App;
