3da0c2784befd2f7d73a0c20fb39a826a79b01ec
gvrsrg <gvrsrg@gmail.com>
2023-08-12 20:32:57 +0300

    react-input ex2

--- week-11/react-input/src/Components/Exercise2.js
+++ week-11/react-input/src/Components/Exercise2.js
@@ -1,26 +1,18 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 const Exercise2 = () => {
     const [name, setName] = useState("");
     const [fruit, setFruit] = useState("");
 
-    const changeFruit = function(e){
-        setFruit(e)
-
-        showFruit(e)
-    }
-
-    const showFruit = function(e){
-        console.log(`${name} chose ${e}`)
-        
-
-    }
-
+    useEffect(() => {
+        console.log(`useEffect: ${name} chose ${fruit}`)
+    }, [name, fruit]
+    )
 
     return (
         <div>
             <input id="name-input" onChange={(e) => setName(e.target.value)} value={name} />
-            <select id="select-input" onChange={(e) => changeFruit(e.target.value)} value={fruit}>
+            <select id="select-input" onChange={(e) => setFruit(e.target.value)} value={fruit}>
                 <option value="banana">banana</option>
                 <option value="mango">mango</option>
                 <option value="pineapple">pineapple</option>
